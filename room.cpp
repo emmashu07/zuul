b7690c2bb96178642816418bfb53a76332b871bd
U-BSD\370801 <370801@SHS-10DECBC5A.beaverton.k12.or.us>
2019-11-21 09:11:56 -0800

    Initial.

--- room.cpp
+++ room.cpp
@@ -4,4 +4,20 @@
 using namespace std;
 
 Room::Room(char* name, char* description) {
-	
+	this.name -> name;
+	this.descriptiom -> description;
+}
+
+Room::~Media() {
+	delete []name;
+	delete []description;
+}
+
+char* Room::getName() {
+	return name;
+}
+
+char* Room::getDescription() {
+	return description;
+}
+

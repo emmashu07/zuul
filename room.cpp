e78f6b839dcfbb07599878d03f456f59409187d0
U-BSD\370801 <370801@SHS-10DECBC5A.beaverton.k12.or.us>
2019-11-21 07:52:34 -0800

    Started

---
+++ room.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "room.h"
+
+using namespace std;
+
+Room::Room(char* name, char* description) {
+	
--- zuul.cpp
+++ zuul.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <vector>
+#include "room.h"
 
 using namespace std;
 
 int main() {
 	vector<Room*> *rooms = new vector<Room*>();
+	
+	cout << "Welcome to Zuul, a text-based adventure game." << endl;
+	cout << "Type HELP to view commands." << endl;
 
+	
 	return 0;
 }

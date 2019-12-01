98b177f841546faeffe1b3f5deeaa4c3fa0c446c
emmashu07 <370801@bsd48.org>
2019-11-30 22:40:06 -0800

    Implement getExit() for rooms

--- item.h
+++ item.h
@@ -1,3 +1,13 @@
+#pragma once
 #include <iostream>
 
 using namespace std;
+
+class Item {
+    public:
+        Item(char* name);
+        virtual ~Item();
+        char* getName();
+    private:
+        char* name;
+};
--- room.h
+++ room.h
@@ -1,15 +1,26 @@
+#pragma once
 #include <iostream>
+#include <vector>
 #include <map>
+#include "item.h"
+
 
 using namespace std;
 
 class Room {
 	public:
 		Room(char* name, char* description);
-		virtual ~Media(); 
+		virtual ~Room(); 
 		char* getName();
 		char* getDescription();
+        void addExit(char* name, Room* room);
+        Room* getExit(char []direction);
+        void addItem(Item* item);
+        void removeItem(Item* item);
+        vector<Item*>* getItems();
 	private:
 		char* name;
 		char* description;
-}
+        map<char*, Room*> exits;
+        vector<Item*> items;
+};
